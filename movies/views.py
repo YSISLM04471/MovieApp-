41e2187e116b5bc06f100dd637aeba31eb5bc281
likhon15 <likhon15-3916@diu.edu.bd>
2024-05-28 13:23:07 +0600

    added

--- movies/views.py
+++ movies/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'movies/index.html', {})
+    context = {
+        'movies': ['interstellar', 'The holiday', 'Alien Movie', 'Rambo IV']
+    }
+    return render(request, 'movies/index.html',context)
 
 def about(request):
     return render(request, 'movies/about.html', {})    
